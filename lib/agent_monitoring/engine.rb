d2f9d3e7a1e8d7f0f01f007c229f1f83097dd7d4
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-29 09:41:38 -0400

    push

--- lib/agent_monitoring/engine.rb
+++ lib/agent_monitoring/engine.rb
@@ -1,7 +1,7 @@
-module AgentMonitoring
+module ForemanPluginTemplate
   class Engine < ::Rails::Engine
-    isolate_namespace AgentMonitoring
-    engine_name 'agent_monitoring'
+    isolate_namespace ForemanPluginTemplate
+    engine_name 'foreman_plugin_template'
 
     config.autoload_paths += Dir["#{config.root}/app/controllers/concerns"]
     config.autoload_paths += Dir["#{config.root}/app/helpers/concerns"]
@@ -9,14 +9,14 @@ module AgentMonitoring
     config.autoload_paths += Dir["#{config.root}/app/overrides"]
 
     # Add any db migrations
-    initializer 'agent_monitoring.load_app_instance_data' do |app|
-      AgentMonitoring::Engine.paths['db/migrate'].existent.each do |path|
+    initializer 'foreman_plugin_template.load_app_instance_data' do |app|
+      ForemanPluginTemplate::Engine.paths['db/migrate'].existent.each do |path|
         app.config.paths['db/migrate'] << path
       end
     end
 
-    initializer 'agent_monitoring.register_plugin', :before => :finisher_hook do |_app|
-      Foreman::Plugin.register :agent_monitoring do
+    initializer 'foreman_plugin_template.register_plugin', :before => :finisher_hook do |_app|
+      Foreman::Plugin.register :foreman_plugin_template do
         requires_foreman '>= 3.7.0'
         register_gettext
 
@@ -24,39 +24,39 @@ module AgentMonitoring
         register_global_js_file 'global'
 
         # Add permissions
-        security_block :agent_monitoring do
-          permission :view_agent_monitoring, {}
+        security_block :foreman_plugin_template do
+          permission :view_foreman_plugin_template, { :'foreman_plugin_template/example' => [:new_action],
+                                                      :'react' => [:index] }
         end
 
-        # Add a new role called 'AgentMonitoring' if it doesn't exist
-        role 'AgentMonitoring', [:view_agent_monitoring]
+        # Add a new role called 'Discovery' if it doesn't exist
+        role 'ForemanPluginTemplate', [:view_foreman_plugin_template]
 
-        # Add menu entry
-        menu :top_menu, :hallas_automation, icon: 'pficon pficon-enterprise', caption: N_('Hallas Automation'), after: :hosts_menu do
-          #submenu :agents_monitoring, caption: N_('Agents Monitoring'), after: :hallas_automation do
-            #menu :agents_one, caption: N_('Agents One')
-            #menu :agents_two, caption: N_('Agents Two')
-          #end
+        # add menu entry
+        sub_menu :top_menu, :plugin_template, icon: 'pficon pficon-enterprise', caption: N_('Plugin Template'), after: :hosts_menu do
+          menu :top_menu, :welcome, caption: N_('Welcome Page'), engine: ForemanPluginTemplate::Engine
+          menu :top_menu, :new_action, caption: N_('New Action'), engine: ForemanPluginTemplate::Engine
         end
 
-        # Add dashboard widget
-        widget 'agent_monitoring_widget', name: N_('Foreman plugin template widget'), sizex: 4, sizey: 1
+        # add dashboard widget
+        widget 'foreman_plugin_template_widget', name: N_('Foreman plugin template widget'), sizex: 4, sizey: 1
       end
     end
 
     # Include concerns in this config.to_prepare block
     config.to_prepare do
+
       begin
-        Host::Managed.send(:include, AgentMonitoring::HostExtensions)
-        HostsHelper.send(:include, AgentMonitoring::HostsHelperExtensions)
+        Host::Managed.send(:include, ForemanPluginTemplate::HostExtensions)
+        HostsHelper.send(:include, ForemanPluginTemplate::HostsHelperExtensions)
       rescue => e
-        Rails.logger.warn "AgentMonitoring: skipping engine hook (#{e})"
+        Rails.logger.warn "ForemanPluginTemplate: skipping engine hook (#{e})"
       end
     end
 
     rake_tasks do
       Rake::Task['db:seed'].enhance do
-        AgentMonitoring::Engine.load_seed
+        ForemanPluginTemplate::Engine.load_seed
       end
     end
   end
