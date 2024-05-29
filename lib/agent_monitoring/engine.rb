27fe69ac829103d1a78f7e075ec69642aa5ef623
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-29 08:36:40 -0400

    push

--- lib/agent_monitoring/engine.rb
+++ lib/agent_monitoring/engine.rb
@@ -35,7 +35,7 @@ module AgentMonitoring
         # add menu entry
         sub_menu :top_menu, :hallas_automation, icon: 'pficon pficon-enterprise', caption: N_('Hallas Automation'), after: :hosts_menu do
           sub_menu :top_menu, :welcome, caption: N_('Agent Monitoring'), parent: :hallas_automation do
-            menu :top_menu, :new_action, caption: N_('Agent One'), :parent => :welcome, engine: AgentMonitoring::Engine
+            menu :top_menu, :new_action, caption: N_('Agent One'), engine: AgentMonitoring::Engine
           end
         end
 
