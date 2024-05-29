f581f7d9c78d988f57c80817ae488b48949fccc0
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-29 15:29:36 -0400

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 AgentMonitoring::Engine.routes.draw do
-  get 'agents', to: 'agents#index', as: 'agents'
-end
-
-Foreman::Application.routes.draw do
-  mount AgentMonitoring::Engine, at: '/agent_monitoring'
+  resources :agents do
+    get :auto_complete_search, on: :collection
+  end
+  
+  namespace :api do
+    namespace :v2 do
+      resources :agents
+    end
+  end
 end
