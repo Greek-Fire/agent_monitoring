e8ce335f6018ff3123227808a445244770bf0264
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-29 18:47:13 -0400

    push

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -2,7 +2,7 @@ module AgentMonitoring
   class AgentsController < ApplicationController
     include Foreman::Controller::AutoCompleteSearch
     def index
-      @agents = Agent.fetch(params)# For now, we'll just render a simple text response
+      @agents = resource_base_search_and_page.search_for(params[:search])
     end
   end
 end
