7829f8541f0920392784521e30c3a90cc620edce
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-29 15:05:01 -0400

    push

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,16 +1,14 @@
 class Agent < ApplicationRecord
-    
-    scoped_search on: :network, complete_value: true
-    scoped_search on: :status, complete_value: true
-    scoped_search on: :agent, complete_value: true
-    scoped_search on: :host
-  
-    def self.fetch(params)
-        list = select_counts
-                .with_counts
-                .search_for(params[:search])
-                .order(params[:order])
-                .paginate(page: params[:page], per_page: params[:per_page])
-        list
-      end   
+  scoped_search on: :network, complete_value: true
+  scoped_search on: :status, complete_value: true
+  scoped_search on: :agent, complete_value: true
+  scoped_search on: :host
+
+  def self.fetch(params)
+    list = self
+            .search_for(params[:search])
+            .order(params[:order])
+            .paginate(page: params[:page], per_page: params[:per_page])
+    list
+  end
 end
