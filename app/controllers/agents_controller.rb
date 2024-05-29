b59d4077288349508e3a3646d84fce367d4b380b
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2024-05-29 15:23:22 -0400

    push

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,14 +1,9 @@
 module AgentMonitoring
   class AgentsController < ApplicationController
     include Foreman::Controller::AutoCompleteSearch
-
+    
     def index
       @agents = Agent.fetch(params)
-      if @agents.empty?
-        render 'welcome'  # Render the welcome view if no agents are found
-      else
-        render 'index'  # Otherwise, render the index view
-      end
     end
   end
 end
